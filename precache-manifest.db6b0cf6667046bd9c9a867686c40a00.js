9b447a8f1b46a57bbe1678639d37f98b9dce1175
Mathew Bryan <bryan.mathew@googlemail.com>
2019-08-13 14:56:45 +0100

    Updates

---
+++ precache-manifest.db6b0cf6667046bd9c9a867686c40a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4b5a6b4cb7acfe26a909b4070f7bb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14c79f74542807fcd917",
+    "url": "/static/css/main.e173e0c7.chunk.css"
+  },
+  {
+    "revision": "5fc75b0a004a05fa4328",
+    "url": "/static/js/2.4b209f10.chunk.js"
+  },
+  {
+    "revision": "14c79f74542807fcd917",
+    "url": "/static/js/main.25bf8b18.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
